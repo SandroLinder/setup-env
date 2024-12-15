a41585b5c11882ae83353c4b4f5c35ab732a6b3f
test <linder.s@gmx.at>
2024-12-15 21:41:01 +0100

    Added setup installing nerdfont

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,6 +3,6 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y gettext lua5.1 liblua5.1-0-dev npm python3 python3-pip \
-    git xclip stow curl libfuse2
+    git xclip stow curl libfuse2 gnome-tweaks
 
 sudo apt autoremove -y
