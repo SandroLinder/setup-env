50459658c117051eca39fa2952c999a9ca61c89d
Sandro <linder.s@gmx.at>
2025-02-21 13:33:00 +0100

    Fixed zig script to download the 64bit version, and updated ghostty install

--- scripts/ghostty.sh
+++ scripts/ghostty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="v1.0.0"
+version="v1.1.2"
 
 if [ ! -z $GHOST_TTY_VERSION ]; then
     version="$GHOST_TTY_VERSION"
@@ -10,6 +10,7 @@ echo "Installing version $version of ghostty"
 
 if [ -d $HOME/Documents/tools/ghostty ]; then
     sudo rm -r $HOME/Documents/tools/ghostty
+    sudo rm -r /usr/local/bin/ghostty
 fi
 
 git clone git@github.com:ghostty-org/ghostty.git $HOME/Documents/tools/ghostty
@@ -19,5 +20,5 @@ git -C $HOME/Documents/tools/ghostty checkout $version
 
 (
     cd $HOME/Documents/tools/ghostty/
-    nice zig build -p $HOME/.local -Doptimize=ReleaseFast
+    sudo nice zig build -p /usr/local -Doptimize=ReleaseFast
 )
--- scripts/install_zig.sh
+++ scripts/install_zig.sh
@@ -6,9 +6,9 @@ if [ -d $HOME/Documents/tools/zig ]; then
 fi
 
 mkdir $HOME/Documents/tools/zig
-curl -o $HOME/Documents/tools/zig/zig.tar.xz https://ziglang.org/download/0.13.0/zig-linux-x86-0.13.0.tar.xz
+curl -o $HOME/Documents/tools/zig/zig.tar.xz https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz 
 
-tar -xf $HOME/Documents/tools/zig/zig.tar.xz -C $HOME/Documents/tools/zig
+tar --verbose -xf $HOME/Documents/tools/zig/zig.tar.xz -C $HOME/Documents/tools/zig
 rm $HOME/Documents/tools/zig/zig.tar.xz
 
-sudo ln -s $HOME/Documents/tools/zig/zig-linux-x86-0.13.0/zig /usr/local/bin/zig
+sudo ln -s $HOME/Documents/tools/zig/zig-linux-x86_64-0.13.0/zig /usr/local/bin/zig
