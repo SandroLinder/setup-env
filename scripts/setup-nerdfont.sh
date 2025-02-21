24a2cb94248b2228eb7886eef3e07f377a870ac3
Sandro <linder.s@gmx.at>
2025-02-21 13:36:41 +0100

    Use install fira code instead of JetBrainsMono

--- scripts/setup-nerdfont.sh
+++ scripts/setup-nerdfont.sh
@@ -4,7 +4,7 @@ if [ -d $HOME/.fonts ]; then
     sudo rm -r $HOME/.fonts
 fi
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 
 unzip -d $HOME/.fonts JetBrainsMono.zip
 rm -r JetBrainsMono.zip
